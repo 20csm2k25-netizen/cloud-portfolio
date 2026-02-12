8756dc5c701ea4438a5d3d8fb13556fb7f34868f
20csm2k25-netizen <20csm2k25@gmail.com>
2026-02-12 13:30:27 +0500

    Add website files

---
+++ script.js
@@ -0,0 +1,9 @@
+function showMessage() {
+    alert("Thank you for visiting my cloud portfolio!");
+}
+
+function scrollToContact() {
+    document.getElementById("contact").scrollIntoView({
+        behavior: "smooth"
+    });
+}
